c48b3592992271ee101b9a5adc7904b1db828760
beshoy hanna <beshoyhanna@outlook.com>
2016-08-29 12:04:41 -0400

    v0.0.0.3
    rename 'encoder' to bottle-nose

---
+++ bottle-nose.js
@@ -0,0 +1,21 @@
+﻿var express = require('express'),
+    app = express(),
+    ffmpeg = require('fluent-ffmpeg');
+
+
+module.exports = {
+    stream: function stream(req, res) {
+        res.contentType('flv');
+        var pathToMovie = '/public/' + req.params.filename;
+        var proc = ffmpeg(pathToMovie)
+            .preset('flashvideo')
+            .on('end', function () {
+                console.log('file has been converted succesfully');
+            })
+            .on('error', function (err) {
+                console.log('an error happened: ' + err.message);
+                res.send(err.message);
+            })
+            .pipe(res, { end: true });
+    }
+}
--- streamserver.js
+++ streamserver.js
@@ -1,10 +1,10 @@
 ﻿var path = require('path'),
     express = require('express'),
     app = express(),
-    encoder = require('./encoder');
+    bottleNose = require('./bottle-nose');
 
     app.get('/api/stream/:filename', function (req, res) {
-        encoder.stream(req, res);
+        bottleNose.stream(req, res);
     });
 
-app.listen(4000);
+    app.listen(4000);
